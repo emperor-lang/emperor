f1e84a61df5eee7f7f0647642cc5bc7a67e98bc8
TheSignPainter <edjones98@gmail.com>
2019-07-17 19:51:42 +0100

    Added optional colour output

--- logging/Logger.hs
+++ logging/Logger.hs
@@ -1,6 +1,6 @@
 module Logger (makeLoggers, Loggers) where
 
-import Args (Args, verbose)
+import Args (Args, verbose, useColour, noUseColour)
 import System.IO (hPutStrLn, stderr)
 
 data LogType = Info
@@ -14,7 +14,7 @@ type Logger = String -> IO ()
 makeLoggers :: Args -> (Logger, Logger, Logger, Logger)
 makeLoggers args = (logError, logInfo, logSuccess, logWarning)
     where
-        logError = makeLogger Error
+        logError = makeLogger args Error
         logInfo = makeVerboseLogger args Info
         logSuccess = makeVerboseLogger args Success
         logWarning = makeVerboseLogger args Warning
@@ -25,12 +25,12 @@ trivialLogger _ = return ()
 makeVerboseLogger :: Args -> LogType -> Logger
 makeVerboseLogger a t = if verbose a
     then 
-        makeLogger t
+        makeLogger a t
     else
         trivialLogger
 
-makeLogger :: LogType -> Logger
-makeLogger t = (hPutStrLn stderr) . colouriseLog t
+makeLogger :: Args -> LogType -> Logger
+makeLogger a t = hPutStrLn stderr . colouriseLog a t
 
 -- logError :: String -> IO ()
 -- logError = makeLogger Error
@@ -52,8 +52,8 @@ makeLogger t = (hPutStrLn stderr) . colouriseLog t
 -- 	SUCCESS:str = '\033[00;32m'
 -- 	WARNING:str = '\033[01;93m'
 
-colouriseLog :: LogType -> String -> String
-colouriseLog t m = messageHeader t ++ " " ++ m
+colouriseLog :: Args -> LogType -> String -> String
+colouriseLog args t m = messageHeader t ++ " " ++ m
     where
         messageHeader :: LogType -> String
         messageHeader t' = colour t' ++ messageHeaderText t' ++ "\x1b[00;00m"
@@ -65,7 +65,10 @@ colouriseLog t m = messageHeader t ++ " " ++ m
         messageHeaderText Success   = "success" ++ ":"
         
         colour :: LogType -> String
-        colour Info     = "\x1b[01;34m"
-        colour Warning  = "\x1b[01;93m"
-        colour Error    = "\x1b[01;31m"
-        colour Success  = "\x1b[01;32m"
+        colour Info     = useANSI args "\x1b[01;34m"
+        colour Warning  = useANSI args "\x1b[01;93m"
+        colour Error    = useANSI args "\x1b[01;31m"
+        colour Success  = useANSI args "\x1b[01;32m"
+
+        useANSI :: Args -> String -> String
+        useANSI a s = if not (noUseColour a) || useColour a then s else ""
