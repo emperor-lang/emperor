fd6fa0a0e6c63af024c0b8953002a5a6dd342744
TheSignPainter <edjones98@gmail.com>
2019-07-19 16:11:13 +0100

    Warn when no input files specified

--- Main.hs
+++ Main.hs
@@ -2,19 +2,27 @@
 
 module Main where
 
-import Args (parseArgv, Args, input)
+import Args (parseArgv, input, verbose)
 import EmperorParserWrapper (parse)
 import Logger (makeLoggers)
 import Formatter (formatFresh)
+import Control.Monad (when)
 
 main :: IO ()
 main = do
     args <- parseArgv
-    (err, inf, scc, _) <- makeLoggers args
+    (err, inf, scc, wrn) <- makeLoggers args
 
-    inf $ "Using input file " ++ input args
-    
-    parseResult <- parse (input args)
+    if input args == ""
+        then do
+            wrn "No input files detected, reading from stdin"
+        else inf $ "Using input file " ++ input args
+
+    let sanitisedArguments = if input args == ""
+        then args { input = "-" }
+        else args
+
+    parseResult <- parse (input sanitisedArguments)
 
     case parseResult of
         Left msg    -> err msg
