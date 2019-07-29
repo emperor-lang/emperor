6739cf226f57462bfbacb399ecea8a07ea34c57c
TheSignPainter <edjones98@gmail.com>
2019-07-29 13:44:12 +0100

    Made subtyping relation a class

--- Types/Checker.hs
+++ Types/Checker.hs
@@ -10,54 +10,62 @@ Language    : Haskell2010
 
 This module defines the type-checker for Emperor
 -}
-module Types.Checker ((|-), (<:)) where
+module Types.Checker ((|-), (<:), SubTypable) where
 
 import Data.List ((\\))
 import Data.Map ((!), empty, keys, Map)
-import Types.Results (EmperorType(..), TypeCheckResult(..))
+import Types.Results (EmperorType(..), TypeCheckResult(..), Purity)
 import Types.Environment (TypeEnvironment)
 
 -- | Represents a comparison between two types
-data TypeComparison = SubType EmperorType EmperorType
+data TypeComparison a b = SubType a b
 
-instance Show TypeComparison where
+instance (Show a, Show b) => Show (TypeComparison a b) where
     show (SubType a b) = show a ++ " <: " ++ show b
 
 -- | Sets up a type-comparison
-(<:) :: EmperorType -> EmperorType -> TypeComparison
+(<:) :: SubTypable a => SubTypable b => a -> b -> TypeComparison a b
 infixl 1 <:
 a <: b = SubType a b
 
--- | Execute a type comparison
-infixl 0 |-
-(|-) :: TypeEnvironment -> TypeComparison -> TypeCheckResult
-_ |- (SubType _ Any) = Pass
-_ |- (SubType Unit _) = Pass
-_ |- (SubType a b)
-    | a == b = Pass
-_ |- (SubType IntP RealP) = Pass
-e |- (SubType (EList a) (EList b)) = e |- (a <: b)
-e |- (SubType (ETuple as) (ETuple bs)) = if all (== Pass) typeResults
-        then Pass
-        else head $ dropWhile (== Pass) typeResults
-    where
-        typeResults = (e |-) <$> comparisons
-        comparisons = (<:) <$> as <*> bs
-e |- (SubType (ERecord s as) (ERecord s' bs))
-    | s == s' 
-        && keys bs `subset` keys as
-        && all (\k -> (e |- ((as ! k) <: (bs ! k))) == Pass) (keys bs)
-        = Pass
-    | otherwise = typeCheckFail (SubType (as ! b) (bs ! b))
+class SubTypable a where
+    infixl 0 |-
+    (|-) :: TypeEnvironment -> TypeComparison a a -> TypeCheckResult
+
+instance SubTypable EmperorType where
+    _ |- (SubType a b)
+        | a == b = Pass
+    _ |- (SubType _ Any) = Pass
+    _ |- (SubType Unit _) = Pass
+    _ |- (SubType IntP RealP) = Pass
+    e |- (SubType (EList a) (EList b)) = e |- (a <: b)
+    e |- (SubType (ETuple as) (ETuple bs)) = if all (== Pass) typeResults
+            then Pass
+            else head $ dropWhile (== Pass) typeResults
         where
-            b = head $ filter (\k -> (e |- ((as ! k) <: (bs ! k))) /= Pass) $ keys as
-e |- (SubType (EFunction i o) (EFunction i' o')) = case e |- (i' <: i) of
-                                                    Pass -> e |- (o <: o')
-                                                    x -> x 
-_ |- c = typeCheckFail c
+            typeResults = (e |-) <$> comparisons
+            comparisons = (<:) <$> as <*> bs
+    e |- (SubType (ERecord s as) (ERecord s' bs))
+        | s == s' 
+            && keys bs `subset` keys as
+            && all (\k -> (e |- ((as ! k) <: (bs ! k))) == Pass) (keys bs)
+            = Pass
+        | otherwise = typeCheckFail (SubType (as ! b) (bs ! b))
+            where
+                b = head $ filter (\k -> (e |- ((as ! k) <: (bs ! k))) /= Pass) $ keys as
+    e |- (SubType (EFunction p i o) (EFunction p' i' o')) = case e |- (p <: p')
+                                                                Pass -> case e |- (i' <: i) of
+                                                                    Pass -> e |- (o <: o')
+                                                                    x -> x 
+                                                                x -> x
+    _ |- c = typeCheckFail c
+
+instance SubTypable Purity where
+    _ |- (SubType Pure Impure) = Fail "Violated type constraint: Pure <: Impure"
+    _ |- _                     = Pass
 
 subset :: Eq a => [a] -> [a] -> Bool
 subset as bs = null $ bs \\ as
 
-typeCheckFail :: TypeComparison -> TypeCheckResult
+typeCheckFail :: Show a => Show b => TypeComparison a b -> TypeCheckResult
 typeCheckFail c = Fail $ "Type-checking error: " ++ show c ++ " does not hold!"
