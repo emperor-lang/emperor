c7b3165057a7d3e02b0ec09def75507cf7f4eefe
TheSignPainter <edjones98@gmail.com>
2019-08-08 12:49:56 +0200

    Added union

--- Types/Environment.hs
+++ Types/Environment.hs
@@ -14,30 +14,34 @@ module Types.Environment
     ( get
     , newTypeEnvironment
     , TypeEnvironment(..)
+    , union
     , unsafeGet
     ) where
 
-import Data.Map (Map, empty, lookup)
+import qualified Data.Map as Map (Map, empty, lookup, union)
 import Data.Maybe (fromMaybe)
 import Prelude hiding (lookup)
 import Types.Results (EmperorType, TypeJudgementResult(..))
 
 -- | An environment which maps names to types
-newtype TypeEnvironment = TypeEnvironment (Map String EmperorType)
+newtype TypeEnvironment = TypeEnvironment (Map.Map String EmperorType)
     deriving (Show)
 
 -- | Creates a fresh type-environment
 newTypeEnvironment :: TypeEnvironment
-newTypeEnvironment = TypeEnvironment empty
+newTypeEnvironment = TypeEnvironment Map.empty
 
 -- | Get a value from a type environment
 get :: String -> TypeEnvironment -> TypeJudgementResult
 get s (TypeEnvironment g) =
-    case lookup s g of
+    case Map.lookup s g of
         Just t -> Valid t
         Nothing -> Invalid $ "Type unknown in current environment" ++ s
         
+union :: TypeEnvironment -> TypeEnvironment -> TypeEnvironment
+union (TypeEnvironment t1) (TypeEnvironment t2) = TypeEnvironment $ Map.union t1 t2
+
 -- | Get a value from a type environment under the assertion that it already
 -- exists. This should only be used for types guaranteed to be in the prelude.
 unsafeGet :: String -> TypeEnvironment -> EmperorType
-unsafeGet s (TypeEnvironment g) = fromMaybe (error $ "Could not find type " ++ s) (lookup s g)
+unsafeGet s (TypeEnvironment g) = fromMaybe (error $ "Could not find type " ++ s) (Map.lookup s g)
