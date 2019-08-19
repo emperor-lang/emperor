54a532a43cb48691558da5702706df819ec3a28f
TheSignPainter <edjones98@gmail.com>
2019-08-19 23:12:46 +0200

    Match types, write headers and get local environments

--- Types/Imports/Imports.hs
+++ Types/Imports/Imports.hs
@@ -14,21 +14,45 @@ From a given file, returns the type environment specified by its interface
 -}
 module Types.Imports.Imports
     ( getEnvironment
+    , getLocalEnvironment
+    , Types.Imports.Imports.writeHeader
     ) where
 
-import AST (Ident(..), Import(..), ImportLocation(..), ImportType(..))
-import Data.Map (filterWithKey)
+import AST (AST(..), Ident(..), Import(..), ImportLocation(..), ImportType(..), ModuleHeader(..), ModuleItem(..), FunctionDef(..), FunctionTypeDef(..))
 import Data.Monoid ((<>))
 import GHC.IO.Exception (ExitCode(..))
 import Logger (Loggers)
 import System.Exit (exitFailure)
 import System.Process (readProcessWithExitCode)
-import Types.Environment (TypeEnvironment(..))
-import Types.Imports.JsonIO (Header(..), isHeaderFile, readHeader)
+import Types.Environment (TypeEnvironment(..), filterEnvironment, insert, newTypeEnvironment)
+import Types.Imports.JsonIO (Header(..), isHeaderFile, readHeader, writeHeader)
+
+writeHeader :: FilePath -> AST -> IO ()
+writeHeader f a = do
+        let g = getLocalEnvironment a
+        let h = generateHeader g a
+        Types.Imports.JsonIO.writeHeader h f
+    where
+        generateHeader :: TypeEnvironment -> AST -> Header
+        generateHeader g' (AST (Module i) is _) = Header i (loc <$> is) g'
+            where
+                loc :: Import -> ImportLocation
+                loc (Import l _) = l
+
+-- | Obtain the type environment created by the content of the module
+getLocalEnvironment :: AST -> TypeEnvironment
+getLocalEnvironment (AST _ _ as) = getLocalEnvironment' as
+    where
+        getLocalEnvironment' :: [ModuleItem] -> TypeEnvironment
+        getLocalEnvironment' [] = newTypeEnvironment
+        getLocalEnvironment' (m:ms) = case m of
+            Component _ _ _ -> error $ "Components have not been implemented for type-checking (and this should have been stopped sooner..." --  getLocalEnvironment ms
+            TypeClass _ _ _ -> error $ "Classes have not been implemented for type-checking (and this should have been stopped sooner..." --  getLocalEnvironment ms
+            FunctionItem (FunctionDef (FunctionTypeDef (Ident i) t) _ _) -> insert i t $ getLocalEnvironment' ms
 
 -- | Given a set of imports, obtain the type environment they form.
 getEnvironment :: Loggers -> [Import] -> IO (Maybe TypeEnvironment)
-getEnvironment _ [] = return Nothing
+getEnvironment _ [] = return $ Just newTypeEnvironment
 getEnvironment (err, inf, scc, wrn) (i:is) = do
     inf $ "Importing " ++ show i
     ti <- getEnvironment' (err, inf, scc, wrn) i
@@ -39,7 +63,9 @@ getEnvironment' :: Loggers -> Import -> IO (Maybe TypeEnvironment)
 getEnvironment' (err, inf, scc, wrn) (Import (ImportLocation t (Ident i)) mis) = do
     e <- getEnvironmentFromFile (err, inf, scc, wrn) t i
     case e of
-        Just g -> return . Just $ filterEnvironment mis g
+        Just g -> case mis of
+            Just is -> return . Just $ filterEnvironment (`elem` ((\(Ident i') -> i') <$> is)) g
+            Nothing -> return . Just $ g
         Nothing -> return Nothing
 
 getEnvironmentFromFile :: Loggers -> ImportType -> FilePath -> IO (Maybe TypeEnvironment)
@@ -69,7 +95,3 @@ getEnvironmentFromFile (err, inf, scc, wrn) Global p = do
                     err x
                     exitFailure
                 Right (Header _ _ g) -> return $ Just g
-
-filterEnvironment :: Maybe [Ident] -> TypeEnvironment -> TypeEnvironment
-filterEnvironment Nothing x = x
-filterEnvironment (Just is) (TypeEnvironment e) = TypeEnvironment $ filterWithKey (\k _ -> Ident k `elem` is) e
