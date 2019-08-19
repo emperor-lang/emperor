0a10ddab6e7fb84af5b423e1f90b2c2ee642da6e
TheSignPainter <edjones98@gmail.com>
2019-08-19 23:12:23 +0200

    Cleaned, call checker

--- Types/Types.hs
+++ Types/Types.hs
@@ -14,25 +14,23 @@ easily.
 module Types.Types
     ( resolveTypes
     , TypeCheckResult(..)
+    , writeHeader
     ) where
 
-import AST (AST(..), ModuleItem(..))
+import AST (AST(..))
 import Logger (Loggers)
-import Types.Environment (TypeEnvironment)
-import Types.Imports.Imports (getEnvironment)
+import Types.Checker ((>-))
+import Types.Imports.Imports (getEnvironment, writeHeader)
 import Types.Results (TypeCheckResult(..))
 
 -- | Find any problems with the typing of results and obtain types if no 
 -- problems are found.
 resolveTypes :: Loggers -> AST -> IO TypeCheckResult
-resolveTypes (err, inf, scc, wrn) (AST _ is bs) = do
+resolveTypes (err, inf, scc, wrn) (AST h is bs) = do
     inf "Getting imports..."
     r <- getEnvironment (err, inf, scc, wrn) is
     case r of
         Just g -> do
             inf $ "Got environment " ++ show g
-            return $ resolveTypes' g bs
-        Nothing -> return $ Fail "Could not get type environment"
-
-resolveTypes' :: TypeEnvironment -> [ModuleItem] -> TypeCheckResult
-resolveTypes' _ bs = Fail $ "Type checking not yet implemented!" ++ show bs
+            return $ g >- (AST h is bs)
+        Nothing -> return $ Fail "Could not get type environment of imports"
