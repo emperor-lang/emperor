f526e8adf3aa16205a25c37a640d4141d9e2256a
Ed Jones <edjones98@gmail.com>
2019-10-09 01:16:43 +0100

    Made error type

---
+++ Errors/Errors.hs
@@ -0,0 +1,22 @@
+module Errors.Errors where
+
+import           Parser.EmperorLexer (AlexPosn)
+
+data Error = Error String AlexPosn
+
+instance Show Error where
+    show (Error m p) = show p ++ ':' : m
+
+data ErrorResult a
+    = Sin [Error]
+    | Dex a
+
+instance Semigroup a => Semigroup (ErrorResult a) where
+    Sin es <> Sin es' = Sin $ es <> es'
+    Dex _ <> Sin es   = Sin es
+    Sin es <> Dex _   = Sin es
+    Dex r <> Dex r'   = Dex $ r <> r'
+
+instance Monoid a => Monoid (ErrorResult a) where
+    mempty = Dex mempty
+
